373febc3fcfcd185afcf53f7394955fe5dd7fff0
เรืองศักดิ์ กุณโฮง <ruengsak84@gmail.com>
2023-01-14 23:49:11 +0700

    lab2

---
+++ .dist/show.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+    $prefix = $_GET["prefix"];
+    $name = $_GET["name"];
+    $age = $_GET["age"];
+    $sex = $_GET["sex"];
+    $status = $_GET["status"];
+    echo "Name:" . $prefix . $name . "<br>";
+    echo "Age:" . $age . "<br>";
+    echo "sex:" . $sex . "<br>";
+    echo "status:" . $status . "<br>";
+    ?>
+</body>
+
+</html>
